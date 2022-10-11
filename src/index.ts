4449b94adbc4b61d6e696b8e666d1ed091d7c577
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-11 16:33:51 -0300

    refactor: move documentation to main file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 type Nullable<T> = T | null;
 
 export class Node {
+    /**
+     * Gets the value of the node.
+     */
     data: number;
+
+    /**
+     * Gets the left child of the node or null.
+     */
     left: Nullable<Node>;
+
+    /**
+     * Gets the right child of the node or null.
+     */
     right: Nullable<Node>;
 
+    /**
+     * Initializes a new Node.
+     *
+     * @param data
+     */
     constructor(data: number) {
         this.data = data;
         this.left = this.right = null;
@@ -12,9 +28,22 @@ export class Node {
 }
 
 export class BST {
+    /**
+     * Gets the root of the tree or null if not set.
+     */
     root: Nullable<Node>;
+
+    /**
+     * Gets the total amount of nodes in the tree.
+     */
     size: number;
 
+    /**
+     * Initializes a new Tree.
+     * Optionally takes a collection of numbers to add to the tree.
+     *
+     * @param collection
+     */
     constructor(collection?: number[]) {
         this.root = null;
         this.size = 0;
@@ -24,6 +53,11 @@ export class BST {
         }
     }
 
+    /**
+     * Adds a new node to the tree with the specified value.
+     *
+     * @param values
+     */
     add(...values: number[]) {
         for (const value of values) {
             this._addNode(value);
@@ -64,6 +98,13 @@ export class BST {
         return search(node);
     }
 
+    /**
+     * Indicates whether a node with the specified value exists.
+     * Optionally takes a `root` node from which to find the value.
+     *
+     * @param value
+     * @param root
+     */
     has(value: number, root: Nullable<Node> = null): boolean {
         let current = root ?? this.root;
 
@@ -78,6 +119,11 @@ export class BST {
         return false;
     }
 
+    /**
+     * Finds the maximum value in the tree.
+     *
+     * @param root
+     */
     max(root: Nullable<Node> = null): number {
         let current = root ?? this.root;
 
@@ -88,6 +134,11 @@ export class BST {
         return current?.data ?? 0;
     }
 
+    /**
+     * Finds the mininum value in the tree.
+     *
+     * @param root
+     */
     min(root: Nullable<Node> = null): number {
         let current = root ?? this.root;
 
@@ -98,6 +149,11 @@ export class BST {
         return current?.data ?? 0;
     }
 
+    /**
+     * Finds a node with the specified value.
+     *
+     * @param value
+     */
     find(value: number): Nullable<Node> {
         let current = this.root;
 
@@ -112,12 +168,22 @@ export class BST {
         return current;
     }
 
+    /**
+     * Indicates whether a node with the given value has no sub-children (i.e is a leaf).
+     *
+     * @param value
+     */
     isLeaf(value: number): boolean {
         const node = this.find(value);
 
         return !node?.right && !node?.left;
     }
 
+    /**
+     * Removes a node from the tree.
+     *
+     * @param values
+     */
     remove(...values: number[]) {
         for (const value of values) {
             const root = JSON.stringify(this.root);
@@ -172,10 +238,21 @@ export class BST {
         return root;
     }
 
+    /**
+     * Gets the height of the tree.
+     * This functions is an alias for the `height` function.
+     *
+     * @param root
+     */
     depth(root: Nullable<Node> = this.root): number {
         return this.height(root);
     }
 
+    /**
+     * Gets the height of the tree.
+     *
+     * @param root
+     */
     height(root: Nullable<Node> = this.root): number {
         if (!root) {
             return -1;
@@ -184,6 +261,9 @@ export class BST {
         return Math.max(this.height(root.left), this.height(root.right)) + 1;
     }
 
+    /**
+     * Indicates whether `root` is balanced.
+     */
     isBalanced(root: Nullable<Node> = this.root): boolean {
         const isHeightBalanced = (node: Nullable<Node>): number => {
             if (!node) {
